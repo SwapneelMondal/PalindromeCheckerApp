fabe64a97db6cc2ec98e4fe87ce60d83b6d43670
SwapneelMondal <swapneelmondal587@gmail.com>
2026-02-27 15:28:28 +0530

    UC2/Palindrome code added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,31 @@
 import java.util.Scanner;
-public class Main{
+
+public class Main {
+
+    public static boolean isPalindrome(String str) {
+        str = str.toLowerCase(); // ignore case
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        //testing commit
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
     }
 }
